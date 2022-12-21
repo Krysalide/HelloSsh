687e60b2cd97e63388138ee3f2e8552a12f93ab6
ChrisDeSimplon <christophe.delapierre.simplon@gmail.com>
2022-12-21 18:58:34 +0100

    Change in the main

--- ProjetJavaSsh/src/Main.java
+++ ProjetJavaSsh/src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
+    public static int num=14;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("quelques valeurs de variables primitives:");
+        // change just to test
+        boolean isTrue= true;
+        System.out.println(isTrue);
+        byte numeric =-128;
+        System.out.println(numeric);
+        char letter= 'Z';
+        System.out.println("lettre Z majuscule:  "+letter);
+        short petitnombre=61;
+        String mot="Toto";
+        System.out.println(mot);
+        int entier=9;
+        System.out.println(entier);
+        long grandNombre=200000000;
+        float pi=3.14159f;
+        double virgule=2.356d;
+        System.out.println("nombre flottant:  "+virgule);
+
+
+
+
     }
 }
